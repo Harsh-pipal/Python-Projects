f7d173d974b9c2b0aae09efc1f7c195df085f324
Harsh-pipal <73478379+Harsh-pipal@users.noreply.github.com>
2021-02-07 02:40:08 +0530

    Update Notification.py

--- Notification.py
+++ Notification.py
@@ -1,11 +1,11 @@
-from plyer import notification
+from plyer import notification    #importing libraries required
 import time
 if __name__=='__main__':
     while True:
         notification.notify(
-            title="Take Rest!",
+            title="Take Rest!",                                                                                                      
             message='Go and have a walk around, your eyes need some rest. Prolonged computer screen gazing may affect your eyes.',
-            app_icon="C:\\Users\\pipal\\OneDrive\\Desktop\\Python Projects\\eyee.ico",
+            app_icon="C:\\Users\\pipal\\OneDrive\\Desktop\\Python Projects\\eyee.ico",                                                #choose your .ico file path
             timeout=10
         )
         time.sleep(2*60*60)
